306e971c62dc42ad1279ffabf4387147c75b5d53
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-04-04 05:53:18 +0530

    Create Question

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Question question = new Question("Who is the most innocent?", new String[]{"Nipuna", "Phi Ball", "Dulanga", "Pue"}, 3);
+        question.printQuestion();
+    }
+}
---
+++ src/Question.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Question {
+    private String question;
+    private String[] answers;
+    private int correctAnswer;
+
+    public Question(String question, String[] answers, int correctAnswer) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public void printQuestion() {
+        System.out.println(this.question);
+        for (int i = 0; i < answers.length; i++) {
+            System.out.println((i + 1) + ") " + answers[i]);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        checkAnswer(choice);
+    }
+
+    public void checkAnswer(int choice) {
+        if (choice == correctAnswer) {
+            System.out.println("Correct");
+        } else {
+            System.out.println("Incorrect");
+        }
+    }
+}
