df58054a92d8367769f2fc273f42019d1f665d14
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-04-04 09:02:29 +0530

    Eliminate static state variable and define state in Paper class

--- src/Question.java
+++ src/Question.java
@@ -4,7 +4,6 @@ public class Question {
     private String question;
     private String[] answers;
     private int correctAnswer;
-    private static int state = 0;
 
     public Question() {
 
@@ -23,19 +22,13 @@ public class Question {
         }
     }
 
-    public void getAnswer() {
+    public int getAnswer() {
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
-        checkAnswer(choice);
+        return choice;
     }
 
-    public void checkAnswer(int choice) {
-        if (choice == correctAnswer) {
-            state++;
-        }
-    }
-
-    public int getState() {
-        return state;
+    public boolean checkAnswer(int choice) {
+        return choice == correctAnswer;
     }
 }
